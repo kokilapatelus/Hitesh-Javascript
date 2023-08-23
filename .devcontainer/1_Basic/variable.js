597f0736ca8da0b0d8beffac2bd818e51320ce11
Chirag Patel <31577043+kokilapatelus@users.noreply.github.com>
2023-08-23 21:57:11 +0000

    variable declaration and output

---
+++ .devcontainer/1_Basic/variable.js
@@ -0,0 +1,14 @@
+const accountId = 123456
+let accountEmail = "chirag@gmail.com"
+var accountPassword =12345
+accountCity = "Jaipur"
+let accountState
+
+// accountId = 2  // not allowed
+accountEmail="cd@cd.com"
+accountPassword= 2314564
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountState);
+console.table([accountId, accountEmail, accountPassword,accountCity]);
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Hitesh-Javascript
+
+
+TypeError: Assignment to constant variable.
+    at Object.<anonymous> (/workspaces/Hitesh-Javascript/.devcontainer/1_Basic/variable.js:6:11)
